4be98d17bc66fa428b19cec4039b4eb11d16fa11
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2013-12-04 15:52:32 -0800

    get FingerTableTest to compile

--- src/test/FingerTableTest.java
+++ src/test/FingerTableTest.java
@@ -8,18 +8,14 @@ import org.junit.Test;
 
 import chord.Finger;
 import chord.FingerTable;
-import chord.RMINode;
-import chord.RMINodeServer;
 
 public class FingerTableTest {
 
 	FingerTable table;
-	RMINodeServer nodeserver;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		nodeserver = new RMINode(2, 0);
-		table = new FingerTable(nodeserver);
+		table = new FingerTable(2, 0);
 	}
 
 	@Test

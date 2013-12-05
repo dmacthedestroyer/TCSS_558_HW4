258d91f45319f397406faa6d423e52fb345b60a6
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2013-12-04 20:58:10 -0800

    Revert "remove GenerateMultiNodeNetwork class"
    This reverts commit d0056df73d44e30937977054fbafd2d9cd745231.

---
+++ src/util/GenerateMultiNodeNetwork.java
@@ -0,0 +1,56 @@
+package util;
+import java.io.IOException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Random;
+
+import chord.RMINode;
+import chord.RMINodeServer;
+
+/**
+ * Script that creates a Chord network with the specified number of nodes.
+ * This is good for testing how the network gets created and whether it's correct.
+ * @author dmac
+ *
+ */
+public class GenerateMultiNodeNetwork {
+	
+	public static void main(String[] args) throws AlreadyBoundException, IOException {
+        if (args.length != 3) {
+            Log.err("Usage: GenerateMultiNodeNetwork <port> <m> <node count>");
+        } else {
+            int port = Integer.parseInt(args[0]);
+			int m = Integer.parseInt(args[1]);
+			int nodeCount = Integer.parseInt(args[2]);
+			
+			long keyspace = (long)Math.pow(2, m); 
+			if(nodeCount > keyspace)
+				throw new IllegalArgumentException("node count (" + nodeCount + ") cannot exceed max number of nodes (" + keyspace + ")");
+			
+			Log.out(String.format("port:%s m:%s node count:%s", port, m, nodeCount));
+			
+            Registry registry = LocateRegistry.createRegistry(port);
+            Log.out("Initialized registry on port " + port);
+            
+            //keep track of already created nodes so we can join nodes to any arbitrary node already in the network
+            ArrayList<RMINodeServer> nodes = new ArrayList<RMINodeServer>();
+            Random random = new Random();
+			
+			for(int i=0; i<nodeCount; i++){
+				RMINode nodeI = new RMINode(m, i);
+				Log.out("" + nodeI.getNodeKey());
+				registry.bind("node" + nodeI.getNodeKey(), UnicastRemoteObject.exportObject(nodeI, 0));
+				
+				RMINodeServer fromNetwork = null;
+				if(nodes.size() > 0)
+					fromNetwork = nodes.get(random.nextInt(nodes.size())); 
+				
+				nodeI.join(fromNetwork);
+				nodes.add(nodeI);
+			}
+        }
+	}
+}
